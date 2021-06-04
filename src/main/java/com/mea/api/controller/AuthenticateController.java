8813af83efe3e5d8f4cdbfd84434dd1279234c5b
fredricksson <bandefredrickson@gmail.com>
2021-06-04 20:32:38 +0100

    make it better

--- src/main/java/com/mea/api/controller/AuthenticateController.java
+++ src/main/java/com/mea/api/controller/AuthenticateController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mea.api.error.ResourceNotFoundException;
@@ -52,5 +53,4 @@ public class AuthenticateController {
 
 		return ResponseEntity.ok(apiResponse);
 	}
-
 }
